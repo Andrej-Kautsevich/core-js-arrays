6c54716a8b8be31b2d65f25bba9b7b95bb500bee
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-12 12:01:12 +0300

    feat: compleate sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -38,8 +38,11 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length >= arr2.length) {
+    return arr1.map((num, idx) => num + (arr2[idx] || 0));
+  }
+  return arr2.map((num, idx) => num + (arr1[idx] || 0));
 }
 
 /**
