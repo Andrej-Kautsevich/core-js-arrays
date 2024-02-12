6a34a640f01a01a71059484f1f3aaf7c04f86896
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-12 15:44:59 +0300

    feat: compleate getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -352,8 +352,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (v, k) => k * 2 + 1);
 }
 
 /**
@@ -368,8 +368,8 @@ function generateOdds(/* len */) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((acc, x) => acc[x], arr);
 }
 
 /**
