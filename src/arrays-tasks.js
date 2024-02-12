83605a6ec19c615b7e9b8c46fa7a903345a9f54d
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-12 16:33:39 +0300

    feat: compleate getHexRGBValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -450,8 +450,10 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map(
+    (el) => `#${`000000${el.toString(16).toUpperCase()}`.slice(-6)}`
+  );
 }
 
 /**
